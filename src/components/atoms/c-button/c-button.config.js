5a9ff8d9ae1ccfd304101f7c094e4536f0b325a0
Michael Gale (he/him) <michael@michaelgale.dev>
2023-05-03 20:11:04 +1000

    fix: dev tools broken when extending chia

--- src/components/atoms/c-button/c-button.config.js
+++ src/components/atoms/c-button/c-button.config.js
@@ -3,3 +3,6 @@
  * Used to sync prop validation (in components) with argTypes (in stories)
  */
 export const colorOptions = ['primary', 'secondary', 'contrast']
+
+// Fix broken dev tools on linux
+export default {}
